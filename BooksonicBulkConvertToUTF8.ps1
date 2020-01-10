42ea4ec7edd193b0d76486e30f5a976b4f31996b
Patrik Johansson <popeen@users.noreply.github.com>
2020-01-10 22:46:01 +0100

    Update BooksonicBulkConvertToUTF8.ps1
    If the script is run multiple times it will now keep all backups

--- BooksonicBulkConvertToUTF8.ps1
+++ BooksonicBulkConvertToUTF8.ps1
@@ -3,7 +3,7 @@ $path = "c:\Audiobooks" #Change this to the path of your library before running 
 Get-ChildItem $path -Recurse|Foreach-Object{
     if($_.Name -eq "desc.txt" -or $_.Name -eq "reader.txt"){
         Write-Host "Converting $($_.FullName)"
-        Copy-Item $_.FullName "$($_.FullName).old" -Force
+        Copy-Item $_.FullName "$($_.FullName).$((get-date -uformat %s).split(".")[0])" -Force
         [System.IO.File]::WriteAllLines($_.FullName, (get-content $_.FullName), (New-Object System.Text.UTF8Encoding($False)))
     }
 }
