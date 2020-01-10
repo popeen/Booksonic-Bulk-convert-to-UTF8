a3650b8cc5c2bd87cb1506cd7756f3c0f5a31e34
Patrik Johansson <popeen@users.noreply.github.com>
2020-01-10 22:17:21 +0100

    Create BooksonicBulkConvertToUTF8.ps1
    This script will first take a backup of all your description and narrator files, then it will convert them to UTF8

---
+++ BooksonicBulkConvertToUTF8.ps1
@@ -0,0 +1,9 @@
+$path = "c:\Audiobooks" #Change this to the path of your library before running the script
+
+Get-ChildItem $path -Recurse|Foreach-Object{
+    if($_.Name -eq "desc.txt" -or $_.Name -eq "reader.txt"){
+        Write-Host "Converting $($_.FullName)"
+        Copy-Item $_.FullName "$($_.FullName).old" -Force
+        [System.IO.File]::WriteAllLines($_.FullName, (get-content $_.FullName), (New-Object System.Text.UTF8Encoding($False)))
+    }
+}
